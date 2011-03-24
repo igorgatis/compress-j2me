fcf782da51919c7b6bc4438f80b784c28e51e598
igorgatis@gmail.com <igorgatis@gmail.com@cff6a726-5fd7-63be-21f2-56cbd07a2b30>
2011-03-24 22:53:20 +0000

    gzip working 2

--- src/gzip-test/test/com/googlecode/compress_j2me/gzip/LinkedHashTest.java
+++ src/gzip-test/test/com/googlecode/compress_j2me/gzip/LinkedHashTest.java
@@ -33,7 +33,7 @@ package com.googlecode.compress_j2me.gzip;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class LinkedHashTest {
+public class LinkedHashTest extends UnitTest {
 
   @Test
   public void testCtor() {
@@ -41,45 +41,74 @@ public class LinkedHashTest {
     Assert.assertEquals(0, hash.size());
   }
 
-//  @Test
-//  public void testPut() {
-//    LinkedHash hash = new LinkedHash(Gzip.DEFLATE_HASH_SIZE);
-//    int k = 0;
-//    for (int i = 0; i < 20; i++, k++) {
-//      Assert.assertEquals(k, hash.put((short) -1, (byte) i, 0));
-//      Assert.assertEquals(k + 1, hash.size());
-//    }
-//    for (int i = 0; i < 20; i++) {
-//      Assert.assertEquals(-1, hash.put((short) -1, (byte) i, 0));
-//      Assert.assertEquals(20, hash.size());
-//    }
-//    for (int i = 0; i < 20; i++) {
-//      for (int j = 0; j < 20; j++, k++) {
-//        Assert.assertEquals(k, hash.put((short) i, (byte) j, 0));
-//        Assert.assertEquals(20 + (i * j), hash.size());
-//      }
-//      for (int j = 0; j < 20; j++) {
-//        Assert.assertEquals(-1, hash.put((short) i, (byte) j, 0));
-//        Assert.assertEquals(20 + (i * 20), hash.size());
-//      }
-//    }
-//    Assert.assertEquals(420, hash.size());
-//  }
+  private void populate(LinkedHash hash) {
+    for (int i = 1; i < 10; i++) {
+      Assert.assertEquals(i, hash.put(0x00, (byte) i, 100 + i));
+      Assert.assertEquals(0xAB00 | i, hash.put(0xAB, (byte) i, 200 + i));
+      Assert.assertEquals(0xABCD00 | i, hash.put(0xABCD, (byte) i, 300 + i));
+    }
+  }
 
-  //  @Test
-  //  public void testReset() {
-  //    Hash hash = new Hash(1 << 16);
-  //    Assert.assertEquals(257, hash.size());
-  //    for (int i = 0; i < 256; i++) {
-  //      Assert.assertEquals(i, hash.putOrGet(-1, (byte) i));
-  //    }
-  //    Assert.assertEquals(-1, hash.putOrGet((char) 0, (byte) 0));
-  //    Assert.assertEquals(258, hash.size());
-  //    Assert.assertEquals(Hash.CLEAR_CODE + 1, hash.putOrGet((char) 0, (byte) 0));
-  //    hash.reset();
-  //    Assert.assertEquals(257, hash.size());
-  //    for (int i = 0; i < 256; i++) {
-  //      Assert.assertEquals(i, hash.putOrGet(-1, (byte) i));
-  //    }
-  //  }
+  @Test
+  public void testReset() {
+    LinkedHash hash = new LinkedHash(Gzip.DEFLATE_HASH_SIZE);
+    Assert.assertEquals(0, hash.size());
+    Assert.assertEquals(0xABCD05, hash.put(0xABCD, (byte) 0x05, 13));
+    Assert.assertEquals(1, hash.size());
+    Assert.assertEquals(13, hash.get(h2b("ABCD05"), 0, 3));
+    hash.reset();
+    Assert.assertEquals(-1, hash.get(h2b("ABCD05"), 0, 3));
+  }
+
+  @Test
+  public void testPut() {
+    LinkedHash hash = new LinkedHash(Gzip.DEFLATE_HASH_SIZE);
+    int k = 0;
+    for (int i = 0; i < 20; i++) {
+      Assert.assertEquals(i, hash.put(0, (byte) i, -1));
+      Assert.assertEquals(0, hash.size());
+    }
+    for (int i = 0; i < 20; i++) {
+      Assert.assertEquals(i, hash.put(0, (byte) i, 0));
+      Assert.assertEquals(++k, hash.size());
+    }
+    for (int i = 0; i < 20; i++) {
+      Assert.assertEquals(i, hash.put(0, (byte) i, 0));
+      Assert.assertEquals(20, hash.size());
+      Assert.assertEquals(i, hash.put(0, (byte) i, 3));
+      Assert.assertEquals(20, hash.size());
+    }
+    for (int i = 0; i < 20; i++) {
+      Assert.assertEquals(0xAB00 | i, hash.put(0xAB, (byte) i, 0));
+      Assert.assertEquals(++k, hash.size());
+      Assert.assertEquals(0xABCD00 | i, hash.put(0xABCD, (byte) i, 0));
+      Assert.assertEquals(++k, hash.size());
+      //Assert.assertEquals(0xABCDEF00 | i, hash.put(0xABCDEF, (byte) i, 0));
+      //Assert.assertEquals(++k, hash.size());
+    }
+  }
+
+  @Test
+  public void testGet() {
+    LinkedHash hash = new LinkedHash(Gzip.DEFLATE_HASH_SIZE);
+    populate(hash);
+
+    Assert.assertEquals(-1, hash.get(h2b("05"), 0, 1));
+    Assert.assertEquals(-1, hash.get(h2b("0005"), 1, 1));
+    Assert.assertEquals(-1, hash.get(h2b("0005"), 0, 2));
+
+    Assert.assertEquals(-1, hash.get(h2b("AB05"), 0, 2));
+    Assert.assertEquals(-1, hash.get(h2b("00AB05"), 1, 2));
+    Assert.assertEquals(-1, hash.get(h2b("AB0005"), 0, 3));
+
+    Assert.assertEquals(305, hash.get(h2b("ABCD05"), 0, 3));
+    Assert.assertEquals(305, hash.get(h2b("ABCD0500"), 0, 3));
+    Assert.assertEquals(305, hash.get(h2b("00ABCD05"), 1, 3));
+    Assert.assertEquals(305, hash.get(h2b("00ABCD0500"), 1, 3));
+    Assert.assertEquals(-1, hash.get(h2b("ABCD0005"), 0, 3));
+    Assert.assertEquals(-1, hash.get(h2b("AB00CD05"), 0, 3));
+    Assert.assertEquals(-1, hash.get(h2b("ABCD0005"), 0, 4));
+    Assert.assertEquals(-1, hash.get(h2b("AB00CD05"), 0, 4));
+  }
+
 }
